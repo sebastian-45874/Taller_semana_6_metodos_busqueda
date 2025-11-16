96b4073bff51d6060da76c82e56f054b1381426b
Vinicio Ruiz <vinicio.ruiz@udla.edu.ec>
2025-11-16 00:17:03 -0500

    Interfaz Ventana agregada

---
+++ Ventana.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Ventana {
+    private JPanel panel;
+    private JList list1;
+    private JTextField txtId;
+    private JButton btnId;
+    private JTextField txtNombre;
+    private JButton btnNombre;
+    private JTextField txtActualizarPrecio;
+    private JButton btnActualizarPrecio;
+    private JComboBox cboMes;
+    private JTextField txtCantidad;
+    private JButton btnMostrar;
+    private JLabel lblId;
+    private JLabel lblNombre;
+    private JLabel lblActualizarPrecio;
+    private JLabel lblRegistrarVenta;
+    private JLabel lblMes;
+    private JLabel lblCantidad;
+}
